e6061f6fc954a892ff37e6253ee9f2342b50f9c1
manwel <manwelwasfystu@gmail.com>
2026-04-20 18:59:34 +0200

    100%

--- methods/linear_system/__init__.py
+++ methods/linear_system/__init__.py
@@ -2,3 +2,4 @@ from .gauss_elimination import GaussElimination
 from .lu_decomposition import LUDecomposition
 from .gauss_jordan import GaussJordan
 from .cramers_rule import CramersRule
+from .gauss_jordan_pivot import GaussJordanPivot
